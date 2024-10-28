965746abbb52ba82dd3a0a0913df412c88e0a1bc
Beta Uliansyah <beta.uliansyah@gmail.com>
2024-10-28 19:52:10 +0700

    Update kode_provinsi.py

--- kode_provinsi.py
+++ kode_provinsi.py
@@ -1,9 +1,9 @@
 import requests
-from bs4 import BeautifulSoup
-import json
-import pandas as pd
 requests.packages.urllib3.disable_warnings() 
-
+from bs4 import BeautifulSoup
+import pandas as pd
+from time import perf_counter
+start_time = perf_counter()
 s = requests.Session()
 r = s.get('https://idm.kemendesa.go.id/rekomendasi', verify=False)
 bsoup = BeautifulSoup(r.text, 'html.parser')
@@ -14,3 +14,5 @@ for i in provinsi_options:
     row_df = pd.DataFrame({'kode_provinsi': [i['value']], 'nama_provinsi': [i['label']]})
     provinsi_df = pd.concat([provinsi_df, row_df], ignore_index=True)
 provinsi_df.to_csv('provinsi.csv')
+total_time = end_time - start_time
+print(f"\n---Finished in: {total_time:.2f} seconds---")
