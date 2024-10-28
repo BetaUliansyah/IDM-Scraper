61d668d0e68400c9b68a99629d2ebb2ced96dd69
Beta Uliansyah <beta.uliansyah@gmail.com>
2024-10-28 19:53:32 +0700

    Create kode_desa.py

---
+++ kode_desa.py
@@ -0,0 +1,22 @@
+import requests
+requests.packages.urllib3.disable_warnings() 
+from bs4 import BeautifulSoup
+import json
+import pandas as pd
+from time import perf_counter
+start_time = perf_counter()
+s = requests.Session()
+kecamatan_df = pd.read_csv('kecamatan.csv')
+kode_kecamatan = kecamatan_df['kode_kecamatan']
+desa_df = pd.DataFrame(columns= ['kode_desa', 'nama_desa'])
+for i in kode_kecamatan:
+    url = 'https://idm.kemendesa.go.id/open/api/lookup/desa/'+str(i)+'?tahun=2024'
+    r = s.get(url, verify=False)
+    response_json = json.loads(r.text)
+    for j in response_json['mapData']:
+        row_df = pd.DataFrame({'kode_desa': [j['id']], 'nama_desa': [j['text']]})
+        desa_df = pd.concat([desa_df, row_df], ignore_index=True)
+desa_df.to_csv('kode_desa.csv')
+end_time = perf_counter()
+total_time = end_time - start_time
+print(f"\n---Finished in: {total_time:.2f} seconds---")
