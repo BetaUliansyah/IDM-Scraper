11c91c437902341b81f0a444cb5c499f48fafa92
Beta Uliansyah <beta.uliansyah@gmail.com>
2024-10-28 19:52:37 +0700

    Create kode_kabkota.py

---
+++ kode_kabkota.py
@@ -0,0 +1,21 @@
+import requests
+requests.packages.urllib3.disable_warnings() 
+from bs4 import BeautifulSoup
+import json
+import pandas as pd
+from time import perf_counter
+start_time = perf_counter()
+s = requests.Session()
+provinsi_df = pd.read_csv('provinsi.csv')
+kode_provinsi = provinsi_df['kode_provinsi']
+kabkota_df = pd.DataFrame(columns= ['kode_kabkota', 'nama_kabkota'])
+for i in kode_provinsi:
+    url = 'https://idm.kemendesa.go.id/open/api/lookup/kabupaten/'+str(i)+'?tahun=2024'
+    r = s.get(url, verify=False)
+    response_json = json.loads(r.text)
+    for j in response_json['mapData']:
+        row_df = pd.DataFrame({'kode_kabkota': [j['id']], 'nama_kabkota': [j['text']]})
+        kabkota_df = pd.concat([kabkota_df, row_df], ignore_index=True)
+kabkota_df.to_csv('kabkota.csv')
+total_time = end_time - start_time
+print(f"\n---Finished in: {total_time:.2f} seconds---")
