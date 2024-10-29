e87dc49f3aaa5b14cf566a8baf1aa543b4e64405
Beta Uliansyah <beta.uliansyah@gmail.com>
2024-10-29 16:13:43 +0700

    Update kode_kecamatan.py

--- kode_kecamatan.py
+++ kode_kecamatan.py
@@ -6,7 +6,7 @@ import pandas as pd
 from time import perf_counter
 start_time = perf_counter()
 s = requests.Session()
-kabkota_df = pd.read_csv('kabkota.csv')
+kabkota_df = pd.read_csv('kode_kabkota.csv')
 kode_kabkota = kabkota_df['kode_kabkota']
 kecamatan_df = pd.DataFrame(columns= ['kode_kecamatan', 'nama_kecamatan'])
 for i in kode_kabkota:
@@ -16,7 +16,7 @@ for i in kode_kabkota:
     for j in response_json['mapData']:
         row_df = pd.DataFrame({'kode_kecamatan': [j['id']], 'nama_kecamatan': [j['text']]})
         kecamatan_df = pd.concat([kecamatan_df, row_df], ignore_index=True)
-kecamatan_df.to_csv('kecamatan.csv')
+kecamatan_df.to_csv('kode_kecamatan.csv')
 end_time = perf_counter()
 total_time = end_time - start_time
 rows = len(kecamatan_df)
